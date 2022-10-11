910fc3cf7aae372a8e825c9a24a78fcda76a1e95
Franco Ezequiel <eze.cavs@hotmail.com>
2022-10-11 16:51:43 -0300

    Subiendo proyecto a github

---
+++ src/app2.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+function App2() {
+  const [test, setTest] =useState([]);
+
+
+  return (
+    <div className="App">
+      Welcome to Wonder Land
+    </div>
+  );
+}
+
+export default App2;
